d51387139eff297238cf795e67c6b2d38741e906
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-01-25 00:47:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+jwplayer("video").setup({
+  width: "100%",
+  height: "100%",
+  autostart: true,
+  mute: false,
+  file: "https://prod-sports-north-gm.jiocinema.com/bpk-tv/Sports18_Khel_voot_MOB/Fallback/Sports18_Khel_voot_MOB-audio_98836_eng=98800-video=1884800.m3u8"
+});
