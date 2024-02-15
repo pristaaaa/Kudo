400984b8187df6c0ecb6561a44042a744c9d581b
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-15 19:23:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ jwplayer("video").setup({
   height: "100%",
   autostart: true,
   mute: false,
-  file: "https://jcevents.akamaized.net/bpk-tv/Sports18_Khel_voot_MOB/Fallback/hdntl=exp=1707918242~acl=%2f*~id=1a061fcded5b4611b0b7c249d8cd334c~data=hdntl~hmac=727820eb907cdfb520fdbae971498264f64c53560c7e5b6ed7f24edbf52e43ca/Sports18_Khel_voot_MOB.m3u8"
+  file: "https://jcevents.akamaized.net/bpk-tv/Sports18_Khel_voot_MOB/Fallback/hdntl=exp=1708091335~acl=%2f*~id=1a061fcded5b4611b0b7c249d8cd334c~data=hdntl~hmac=0b1cc2133bf65a7033c0de58c3b8e367753145c09b03f6b7ec08fcc4d27c1b2b/Sports18_Khel_voot_MOB.m3u8"
 });
